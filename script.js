1f21e0c4648158d8491303d2ecc12be7b14e5744
Fundorin174 <43452208+Fundorin174@users.noreply.github.com>
2019-06-09 19:39:01 +0300

    09june

---
+++ script.js
@@ -0,0 +1,10 @@
+// Клик по меню
+
+menu_btn.addEventListener('click', menuStart); 
+
+// Превращение меню в стрелку и обратно
+function menuStart() {
+  document.getElementById('menu_btn').classList.toggle('menu_btn_active');
+  }
+
+  
