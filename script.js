1781e947a2ffde5e288618588450e2270db06148
Fundorin174 <Fundorin174@gmail.com>
2019-06-23 12:11:56 +0300

    mobile menu

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+let widScr = window.innerWidth;//Ширина окна браузера
+
+
 // Клик по меню
 
 menu_btn.addEventListener('click', menuStart); 
+
+menu_btn.addEventListener('click', appearingMenu); 
+
+link_main.addEventListener('click', chekMobile);
+link_about.addEventListener('click', chekMobile);
+link_gallery.addEventListener('click', chekMobile);
+link_service.addEventListener('click', chekMobile);
+link_contact.addEventListener('click', chekMobile);
+  
+// Исчезновение меню по нажатию ссылки
+function chekMobile(){
+    if (widScr<600) {
+      appearingMenu();
+      menuStart();
+    } 
+}
 
 // Превращение меню в стрелку и обратно
 function menuStart() {
   document.getElementById('menu_btn').classList.toggle('menu_btn_active');
   }
 
-  
+ // Появление мобильного меню
+function appearingMenu() {
+  document.getElementById('Topnav').classList.toggle('links_active');
+} 
+
+
+
